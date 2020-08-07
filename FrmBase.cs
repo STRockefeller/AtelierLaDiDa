fb01e425ee67b75a9a14d51cf3b93bc805a38607
Rockefeller <kenny71223@gmail.com>
2020-08-07 15:07:11 +0800

    20200807-update
    新增A20，修正了下拉式選單累加問題。

--- FrmBase.cs
+++ FrmBase.cs
@@ -27,6 +27,7 @@ namespace AtelierLaDiDa
         private void FrmBase_Load(object sender, EventArgs e)
         {
             cbxSeriesName.Items.Add("A17");
+            cbxSeriesName.Items.Add("A20");
             cbxSeriesName.Items.Add("A21");
             //to update
         }
@@ -38,6 +39,10 @@ namespace AtelierLaDiDa
                 case "A17":
                     lblSeriesName.Text = "ソフィーのアトリエ ～不思議な本の錬金術士～";
                     seriesName = EnumSeriesName.A17_Atelier_Sophie;
+                    break;
+                case "A20":
+                    lblSeriesName.Text = "ルルアのアトリエ ～アーランドの錬金術士 4～";
+                    seriesName = EnumSeriesName.A20_Atelier_Lulua;
                     break;
                 case "A21":
                     lblSeriesName.Text = "ライザのアトリエ 〜常闇の女王と秘密の隠れ家〜";
@@ -55,11 +60,20 @@ namespace AtelierLaDiDa
         {
             List<string> returnValue;
             string[] objectList;
+            cbxDestination.Items.Clear();
+            cbxSource.Items.Clear();
             switch (seriesName)
             {
                 case EnumSeriesName.A17_Atelier_Sophie:
                     DataBases.A17DB a17 = new DataBases.A17DB();
                     returnValue = a17.generateObjectList();
+                    objectList = returnValue.ToArray();
+                    cbxSource.Items.AddRange(objectList);
+                    cbxDestination.Items.AddRange(objectList);
+                    break;
+                case EnumSeriesName.A20_Atelier_Lulua:
+                    A20DB a20 = new A20DB();
+                    returnValue = a20.generateObjectList();
                     objectList = returnValue.ToArray();
                     cbxSource.Items.AddRange(objectList);
                     cbxDestination.Items.AddRange(objectList);
@@ -81,11 +95,13 @@ namespace AtelierLaDiDa
             indexUpdate();
         }
 
+        #region test
         private void btnTest_Click(object sender, EventArgs e)
         {
             AtelierLaDiDa.DataBases.A17DB a17 = new DataBases.A17DB();
             a17.testDB();
         }
+        #endregion
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
@@ -94,6 +110,10 @@ namespace AtelierLaDiDa
                 case EnumSeriesName.A17_Atelier_Sophie:
                     A17DB a17 = new A17DB();
                     tbxResult.Text = a17.search(cbxSource.Text,cbxDestination.Text);
+                    break;
+                case EnumSeriesName.A20_Atelier_Lulua:
+                    A20DB a20 = new A20DB();
+                    tbxResult.Text = a20.search(cbxSource.Text, cbxDestination.Text);
                     break;
                 case EnumSeriesName.A21_Atelier_Ryza:
                     A21DB a21 = new A21DB();
